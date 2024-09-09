d01dbbc36b5cc68d963dd90fe7e24d7cbf582c9e
santino <santinofernandez67@gmail.com>
2024-09-09 17:44:53 -0300

    cambios

--- src/models/inscripcionesModel.ts
+++ src/models/inscripcionesModel.ts
@@ -4,18 +4,18 @@ import { Estudiante } from "./estudianteModel";
 
 @Entity('cursos_estudiantes')
 export class Inscripcion extends BaseEntity {
-    @PrimaryColumn()  // Establecemos curso_id como parte de la clave primaria compuesta
+    @PrimaryColumn()  
     curso_id: number;
 
-    @PrimaryColumn()  // Establecemos estudiante_id como parte de la clave primaria compuesta
+    @PrimaryColumn()  
     estudiante_id: number;
 
     @ManyToOne(() => Curso)
-    @JoinColumn({ name: 'curso_id' })  // Asociar con la columna curso_id
+    @JoinColumn({ name: 'curso_id' })  
     curso: Curso;
 
     @ManyToOne(() => Estudiante)
-    @JoinColumn({ name: 'estudiante_id' })  // Asociar con la columna estudiante_id
+    @JoinColumn({ name: 'estudiante_id' })  
     estudiante: Estudiante;
 
     @Column('float')
